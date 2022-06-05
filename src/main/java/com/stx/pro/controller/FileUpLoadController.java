acbcb15b62fa2eb218861ddadd6ef127ef9f6653
RenBoQing <2098510757@qq.com>
2022-06-05 17:59:47 +0800

    RenBoQing

--- src/main/java/com/stx/pro/controller/FileUpLoadController.java
+++ src/main/java/com/stx/pro/controller/FileUpLoadController.java
@@ -2,11 +2,14 @@ package com.stx.pro.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 /**
  * @author RenBoQing
  * @date 2022年05月30日 16:37
- * @Description 项目文件上传 Controller
+ * @Description 文件上传 Controller
  */
 @RequestMapping("/upload")
 public class FileUpLoadController {
