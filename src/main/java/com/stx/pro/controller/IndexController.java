ddb335ecb5013fc9e58e13a720d8c7781c8d4e13
RenBoQing <2098510757@qq.com>
2022-06-08 11:32:00 +0800

    Changes

--- src/main/java/com/stx/pro/controller/IndexController.java
+++ src/main/java/com/stx/pro/controller/IndexController.java
@@ -43,7 +43,7 @@ public class IndexController {
      * @date 2022/6/1 0001 20:41
      * @return java.lang.String
      */
-    @RequestMapping("/admindSummary")
+    @RequestMapping("/summary")
     public String summary() {
         return "/summary";
     }
